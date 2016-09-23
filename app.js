61fa59caf99870ae1697e9307c89495bd4b49273
hstelle <stellehenry@gmail.com>
2016-09-23 13:22:05 -0700

    question2product

--- app.js
+++ app.js
@@ -9,11 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a,b){ //eslint-disable-line
-
+  var sum = a + b;
+  var message = 'The sum of ' + a + ' and ' + b + ' is ' + sum + '.';
+  var output = [sum, message];
+  return output;
 }
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -26,12 +29,15 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-function multiply(a,b){ //eslint-disable-line
-
+function multiply(a,b) {
+  var multiply = a * b;
+  var message = 'The product of ' + a + ' and ' + b + ' is ' + multiply + '.'; //eslint-disable-line
+  var output = [multiply, message];
+  return output;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -48,7 +54,7 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+  var sumAndMultiply = a 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
